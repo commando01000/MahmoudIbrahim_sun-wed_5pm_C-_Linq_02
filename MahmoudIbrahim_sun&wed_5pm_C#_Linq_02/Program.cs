f406e161a9acd754b32590c8f6c7264954484670
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-08-07 01:46:09 +0300

    Update Program.cs

--- MahmoudIbrahim_sun&wed_5pm_C#_Linq_02/Program.cs
+++ MahmoudIbrahim_sun&wed_5pm_C#_Linq_02/Program.cs
@@ -60,33 +60,64 @@ namespace MahmoudIbrahim_sun_wed_5pm_C__Linq_02
             //Console.WriteLine($"The total sum of the array is: {Total}");
             // 5.
             // Read all lines from the file into an array of strings
-            string[] words = File.ReadAllLines("dictionary_english.txt");
+            //string[] words = File.ReadAllLines("dictionary_english.txt");
 
             // Calculate the total number of characters
-            int totalCharacters = words.Sum(word => word.Length);
+            //int totalCharacters = words.Sum(word => word.Length);
 
             // Print the total number of characters
-            Console.WriteLine($"The total number of characters in all words is: {totalCharacters}");
+            //Console.WriteLine($"The total number of characters in all words is: {totalCharacters}");
 
-            // 6.
-            string filePath = "dictionary_english.txt";
+            // 6. & 7. & 8.
+            //string filePath = "dictionary_english.txt";
 
-            try
-            {
-                using (StreamReader reader = new StreamReader(filePath))
-                {
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        // Process the line (for now, just print it)
-                        Console.WriteLine(line);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"An error occurred: {e.Message}");
-            }
+            //try
+            //{
+            //    using (StreamReader reader = new StreamReader(filePath))
+            //    {
+            //        string line;
+            //        int min = int.MaxValue;
+            //        int max = int.MinValue;
+            //        int totalCharacters = 0;
+            //        int wordCount = 0;
+
+            //        while ((line = reader.ReadLine()) != null)
+            //        {
+            //            int lineLength = line.Length;
+            //            totalCharacters += lineLength;
+            //            wordCount++;
+
+            //            if (lineLength < min)
+            //            {
+            //                min = lineLength;
+            //            }
+
+            //            if (lineLength > max)
+            //            {
+            //                max = lineLength;
+            //            }
+            //        }
+
+            //        double average = wordCount == 0 ? 0 : (double)totalCharacters / wordCount;
+
+            //        Console.WriteLine($"The minimum number of characters in a word is: {min}");
+            //        Console.WriteLine($"The maximum number of characters in a word is: {max}");
+            //        Console.WriteLine($"The average length of the words is: {average:F2}");
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine($"An error occurred: {e.Message}");
+            //}
+
+            // 9.
+            
+            //var TotalUnitsInStock = ProductList.GroupBy(product => product.Category).Select(group => new { CategoryName = group.Key, TotalUnitsInStock = group.Sum(product => product.UnitsInStock) }).ToList();
+
+            //foreach (var category in TotalUnitsInStock)
+            //{
+            //    Console.WriteLine($"Category: {category.CategoryName}, Total Units in Stock: {category.TotalUnitsInStock}");
+            //}
             #endregion
         }
     }
